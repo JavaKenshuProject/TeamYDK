75974b5a8a38608de0ca62c8c822814b123bae3e
embex-user <embex-user@192.168.128.101>
2017-05-31 15:34:17 +0900

    vol2

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,29 +15,59 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
 
+		//エンコーディング指定
+		 request.setCharacterEncoding("Windows-31J");
+	     response.setCharacterEncoding("Windows-31J");
+
+	     // formからの値を取得
+	     String login = request.getParameter("login");
+
+
+	     // 移譲する先のjspを格納する変数url
+	     String url = null;
+
+
+	     if(login.equals("ログイン")){
+
+	    	 //パラメーター取得
+	    	 String userID = request.getParameter("userID");
+		     String password = request.getParameter("password");
+
+/*		     //DAO,Beanをインスタンス化
+  			 ArrayList<UserBean> userList = new ArrayList<UserBean>();
+		     UserDAO dao = new UserDAO();
+
+		     //DAOからのreturnをBeanに格納
+		     try{
+		     	userList=dao.userAllGet()
+		     }catch(Exception e){
+		     	e.printStackTrace
+		     }
+
+
+		     //urlに格納する
+		     if(userID.equals()&&password.equals()){
+		    	 url="Menu.jsp";
+		     }else{
+		    	 //エラー画面へ
+		    	 url="error_seivlet.jsp";
+		     }
+*/
+
+	     }
+
+	     RequestDispatcher rd = request.getRequestDispatcher(url);
+	        rd.forward(request, response);
+	}
 }
