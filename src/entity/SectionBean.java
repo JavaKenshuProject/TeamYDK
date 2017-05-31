a8ba161fec4d4a0494aa4372a8ee856fbc5c165a
波多　駿 <@192.168.128.101>
2017-05-31 14:53:36 +0900

    SectionBean Vol.1

--- src/entity/SectionBean.java
+++ src/entity/SectionBean.java
@@ -1,5 +1,31 @@
 package entity;
 
-public class SectionBean {
+import java.io.Serializable;
 
+import com.sun.jmx.snmp.Timestamp;
+
+public class SectionBean implements Serializable {
+
+	private String section_code;
+	private String section_name;
+	private Timestamp update_date;
+
+	public String getSection_code() {
+		return section_code;
+	}
+	public void setSection_code(String section_code) {
+		this.section_code = section_code;
+	}
+	public String getSection_name() {
+		return section_name;
+	}
+	public void setSection_name(String section_name) {
+		this.section_name = section_name;
+	}
+	public Timestamp getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(Timestamp update_date) {
+		this.update_date = update_date;
+	}
 }
