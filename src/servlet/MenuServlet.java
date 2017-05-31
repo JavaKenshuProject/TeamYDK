857936e0797b8f005bd0e9f617c415cdf0912478
embex-user <embex-user@192.168.128.104>
2017-05-31 18:21:40 +0900

    menuServlet ver.1

---
+++ src/servlet/MenuServlet.java
@@ -0,0 +1,77 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MenuServlet
+ */
+@WebServlet("/MenuServlet")
+public class MenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MenuServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+
+		//エンコーディング指定
+		 request.setCharacterEncoding("Windows-31J");
+	     response.setCharacterEncoding("Windows-31J");
+
+	     // formからの値を取得
+	     String page = request.getParameter("page");
+
+	     // 移譲する先のjspを格納する変数url
+	     String url = null;
+
+	     //メニューボタンの値から行き先をurlに格納する
+	    if(page.equals("従業員登録")){
+	    	url = "EmployeeInsert.jsp";
+	    }
+
+	    if(page.equals("資格取得")){
+	    	url = "LicenseGet.jsp";
+	    }
+
+	    if(page.equals("資格追加")){
+	    	url = "LicenseInsert.jsp";
+	    }
+
+	    if(page.equals("ユーザ登録")){
+	    	url = "UserInsert.jsp";
+	    }
+
+	    if(page.equals("ユーザ登録")){
+	    	url = "UserInsert.jsp";
+	    }
+
+	    RequestDispatcher rd = request.getRequestDispatcher(url);
+	    rd.forward(request, response);
+	}
+
+}
