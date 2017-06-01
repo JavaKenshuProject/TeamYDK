ce4bdc20313603fe2c235757fc8dcd8746cbf702
embex-user <embex-user@192.168.128.102>
2017-06-01 15:51:22 +0900

    setattribute

--- src/servlet/ShowServlet.java
+++ src/servlet/ShowServlet.java
@@ -65,6 +65,7 @@ public class ShowServlet extends HttpServlet {
 
 	    if(page.equals("変更")){
 		url = "ChangeServlet";
+		request.setAttribute(page, page);
 	}
 	/* 転送先 */
 	RequestDispatcher rd = request.getRequestDispatcher(url);
