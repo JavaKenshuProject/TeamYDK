879b492804922bb219c1d625e5f1b32ee10abaf1
embex-user <embex-user@192.168.128.100>
2017-06-01 10:29:30 +0900

    EmployeeDAO及びCheckDormatを追加しました

---
+++ src/dao/CheckFormat.java
@@ -0,0 +1,304 @@
+package dao;
+
+import java.nio.charset.Charset;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import entity.EmployeeBean;
+import entity.LicenseBean;
+import entity.UserBean;
+import exception.ServletServiceException;
+
+public class CheckFormat {
+
+	/**
+	 * 指定された文字エンコーディングでのバイト数をintで返します
+	 *
+	 * @param string
+	 * @param charset
+	 * @return int
+	 */
+	public static int getByteLength(String string, Charset charset) {
+		return string.getBytes(charset).length;
+	}
+
+	/**
+	 * 指定された文字エンコーディングでのバイト数をカウントします。 バイト数が引数sizeを超えていた場合、falseを返します。
+	 *
+	 * @param string
+	 * @param charset
+	 * @param size
+	 * @return boolean
+	 */
+	public static boolean checkSize(String string, Charset charset, int size) {
+		boolean flag;
+		if (getByteLength(string, charset) <= size) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		return flag;
+	}
+
+	/**
+	 * Windows-31Jでのバイト数をカウントします。 バイト数が引数sizeを超えていた場合、falseを返します。
+	 *
+	 * @param string
+	 * @param size
+	 * @return boolean
+	 */
+	public static boolean checkSize31J(String string, int size) {
+		boolean flag;
+		Charset charset = Charset.forName("Windows-31J");
+		if (getByteLength(string, charset) <= size) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+		return flag;
+	}
+
+	/**
+	 * Date型の定数です
+	 */
+	static public final String DATE_PATTERN = "yyyy-MM-dd";
+	//String DATE_PATTERN ="yyyy/MM/dd HH:mm:ss";
+
+	/**
+	 * 文字列型のDate型としての妥当性を精査します
+	 *
+	 * @param date
+	 * @return boolean
+	 */
+	public static boolean checkDate(String date) {
+		DateFormat df = new SimpleDateFormat(DATE_PATTERN);
+		df.setLenient(false);
+		try {
+			df.parse(date);
+			return true;
+		} catch (ParseException e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Date型のオブジェクトをString型に変換します
+	 * @param date
+	 * @return
+	 */
+	public static String convertDate2String(Date date){
+		String  str;
+		if(date == null) {
+			str = null;
+		} else {
+			str = new SimpleDateFormat(DATE_PATTERN).format(date);
+		}
+		return str;
+	}
+
+	/**
+	 * String型のオブジェクトをDate型に変換します---変換前にcheckDateによる妥当性の精査が行われます
+	 * ---変換不可能な場合はnullを返します
+	 *
+	 * @param source
+	 * @return Date
+	 */
+	public static java.sql.Date convertString2Date(String source) {
+		if ((source != null) && (checkDate(source)) ) {
+			Date date = Date.valueOf(source);
+			return date;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * EmployeeBeanの中身がDBのテーブル内と適合しているかチェックします     NULLcheck及びDATA型など検証が足りない部分あり!!!要修正!!!
+	 *
+	 * @param employee
+	 * @return void or throw
+	 * @throws ServletServiceException
+	 */
+	public static void checkEmployeeBean(EmployeeBean employee) throws ServletServiceException {
+		String KATAKANA = "^[\\u30A0-\\u30FF]+$";
+
+		String call = "";
+		String temp = "が不正です\n";
+
+		if ((employee.getEmp_code() == null) || (checkSize31J(employee.getEmp_code(), 4) == false)) {
+			call = call + "従業員コード" + temp;
+		}
+
+		if ((employee.getL_name() == null) || (checkSize31J(employee.getL_name(), 16) == false)) {
+			call = call + "氏" + temp;
+		}
+
+		if ((employee.getF_name() == null) || (checkSize31J(employee.getF_name(), 16) == false)) {
+			call = call + "名" + temp;
+		}
+
+		if ((employee.getL_kana_name() == null) || (checkSize31J(employee.getL_kana_name(), 24)) == false
+				|| employee.getL_kana_name().matches(KATAKANA) == false) {
+			call = call + "氏(カナ)" + temp;
+		}
+
+		if ((employee.getF_kana_name() == null) ||(checkSize31J(employee.getF_kana_name(), 24)) == false
+				|| employee.getF_kana_name().matches(KATAKANA) == false) {
+			call = call + "名(カナ)" + temp;
+		}
+
+		if (!(employee.getSex() == 0 || employee.getSex() == 1)) {
+			call = call + "性別" + temp;
+		}
+
+		if ((employee.getBirth_day() == null) || (checkDate(employee.getBirth_day()) == false)) {
+			call = call + "所属コード" + temp;
+		}
+
+		if ((employee.getSection_code() == null) || (checkSize31J(employee.getSection_code(), 2) == false)) {
+			call = call + "所属コード" + temp;
+		}
+
+		if ((employee.getEmp_date() == null) || (checkDate(employee.getEmp_date()) == false)) {
+			call = call + "所属コード" + temp;
+		}
+
+
+		if (!(call.equals(""))) {
+			throw new ServletServiceException(call);
+		}
+	}
+
+	/**
+	 * UserBeanの中身がDBのテーブル内と適合しているかチェックします
+	 *
+	 * @param user
+	 * @return void or throw
+	 * @throws ServletServiceException
+	 */
+	public static void checkUserBean(UserBean user) throws ServletServiceException {
+		String call = "";
+		String temp = "が不正です\n";
+
+		if ((user.getUser_id() == null) || (checkSize31J(user.getUser_id(), 24) == false)) {
+			call = call + "ユーザID" + temp;
+		}
+
+		if ((user.getPassword() == null) || (checkSize31J(user.getPassword(), 32) == false)) {
+			call = call + "パスワード" + temp;
+		}
+
+		if (!(call.equals(""))) {
+			throw new ServletServiceException(call);
+		}
+	}
+
+	/**
+	 * LicenseBeanの中身がDBのテーブル内と適合しているかチェックします
+	 *
+	 * @param license
+	 * @return void or throw
+	 * @throws ServletServiceException
+	 */
+	public static void checkLicenseBean(LicenseBean license) throws ServletServiceException {
+		String call = "";
+		String temp = "が不正です\n";
+
+		if ((license.getLicense_cd() == null) || (checkSize31J(license.getLicense_cd(), 5) == false)) {
+			call = call + "資格コード" + temp;
+		}
+
+		if ((license.getLicense_name() == null) || (checkSize31J(license.getLicense_name(), 100) == false)) {
+			call = call + "資格名" + temp;
+		}
+
+		if (!(call.equals(""))) {
+			throw new ServletServiceException(call);
+		}
+	}
+
+	/**
+	 * emp_Codeの重複を検査します
+	 * (employee(登録する従業員情報),ArrayList(全従業員情報))
+	 * 重複していた場合はfalseを返します
+	 * @param employee
+	 * @throws ServletServiceException
+	 */
+	public static boolean checkPK_empCode(EmployeeBean employee,ArrayList<EmployeeBean> emp_list) {
+		 boolean flag = true;
+		for (EmployeeBean all_emp : emp_list) {
+			if (all_emp.getEmp_code().equals(employee.getEmp_code())) {
+				flag = false;;
+				//throw new ServletServiceException("従業員コードが重複しています");
+			}
+		}
+		return flag;
+	}
+
+
+	/**
+	 * emp_Codeとlicense_cdの重複を検査します
+	 * (employee(登録する従業員情報),ArrayList(全従業員情報))
+	 * 重複していた場合はfalseを返します
+	 * @param employee
+	 * @throws ServletServiceException
+	 */
+	public static boolean checkPK_t_get_license(EmployeeBean employee,ArrayList<EmployeeBean> emp_list) {
+		boolean flag = true;
+		for (EmployeeBean all_emp : emp_list) {
+			for(String string : all_emp.getLicense_cd()){
+				for(String string2 : all_emp.getLicense_name()){
+					if (string.equals(employee.getLicense_cd_SQLinsert()) && string2.equals(employee.getLicense_name())) {
+						flag = false;
+						//throw new ServletServiceException("すでに登録された資格です");
+					}
+				}
+			}
+
+		}
+		return flag;
+	}
+
+
+	/**
+	 * license_cdの重複を検査します
+	 * (license(登録する従業員情報),ArrayList(全従業員情報))
+	 * 重複していた場合はfalseを返します
+	 * @param license
+	 * @throws ServletServiceException
+	 */
+	public static boolean checkPK_license(LicenseBean license,ArrayList<LicenseBean> license_list) {
+		boolean flag = true;
+		for (LicenseBean all_license : license_list) {
+			if (all_license.getLicense_cd().equals(license.getLicense_cd())) {
+				flag = false;
+				//throw new ServletServiceException("資格コードが重複しています");
+			}
+		}
+		return flag;
+	}
+
+
+	/**
+	 * user_idの重複を検査します
+	 * (user(登録する従業員情報),ArrayList(全従業員情報))
+	 * 重複していた場合はfalseを返します
+	 * @param user
+	 * @throws ServletServiceException
+	 */
+	public static boolean checkPK_user(UserBean user,ArrayList<UserBean> user_list) {
+		boolean flag = true;
+		for (UserBean all_user : user_list) {
+			if (all_user.getUser_id().equals(user.getUser_id())) {
+				flag = false;
+				//throw new ServletServiceException("ユーザIDが重複しています");
+			}
+		}
+		return flag;
+	}
+
+
+}
--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -1,10 +1,14 @@
 package dao;
 
-import java.sql.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import entity.EmployeeBean;
 import exception.ServletServiceException;
+
 
 /**
  *
@@ -12,65 +16,291 @@ import exception.ServletServiceException;
  * @version 1.00
  */
 public class EmployeeDAO {
+
+	/**
+	 * employeeAllGetから呼ばれるメソッドです
+	 *
+	 * @param con
+	 * @param employee
+	 * @return
+	 * @throws SQLException
+	 */
+	private EmployeeBean m_sectionGet(Connection con, EmployeeBean employee) throws SQLException {
+		String section_sql = "SELECT * FROM m_section WHERE section_code = ?";
+
+		try (PreparedStatement section_pstmt = con.prepareStatement(section_sql);) {
+
+			section_pstmt.setString(1, employee.getSection_code());
+			try (ResultSet section_res = section_pstmt.executeQuery();) {
+				if (section_res.next()) {
+					// m_section
+					employee.setSection_name(section_res.getString("section_name"));
+					employee.setSection_update_date(section_res.getTimestamp("update_date"));
+				}
+			} catch (SQLException e) {
+				throw e;
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return employee;
+
+	}
+
+	/**
+	 * employeeAllGetから呼ばれるメソッドです
+	 *
+	 * @param con
+	 * @param employee
+	 * @return
+	 * @throws SQLException
+	 */
+	private EmployeeBean t_get_licenseGet(Connection con, EmployeeBean employee) throws SQLException {
+		String get_license_sql = "SELECT * FROM t_get_license WHERE emp_code = ?";
+		try (PreparedStatement get_license_pstmt = con.prepareStatement(get_license_sql);) {
+
+			get_license_pstmt.setString(1, employee.getEmp_code());
+			try (ResultSet get_license_res = get_license_pstmt.executeQuery();) {
+				ArrayList<String> license_cd = new ArrayList<String>();
+				ArrayList<String> license_date = new ArrayList<String>();
+
+				while (get_license_res.next()) {
+					license_cd.add(get_license_res.getString("license_cd"));
+					license_date.add(CheckFormat.convertDate2String(get_license_res.getDate("get_license_date")));
+				}
+
+				// t_get_license
+				employee.setLicense_cd(license_cd);
+				employee.setGet_license_date(license_date);
+			} catch (SQLException e) {
+				throw e;
+			}
+		} catch (SQLException e) {
+			throw e;
+		}
+		return employee;
+	}
+
+	/**
+	 * employeeAllGetから呼ばれるメソッドです
+	 *
+	 * @param con
+	 * @param employee
+	 * @return
+	 * @throws SQLException
+	 */
+	private EmployeeBean m_licenseGet(Connection con, EmployeeBean employee) throws SQLException {
+
+		String license_sql = "SELECT * FROM m_license WHERE license_cd = ?";
+		ArrayList<String> license_name = new ArrayList<String>();
+		try (PreparedStatement license_pstmt = con.prepareStatement(license_sql);) {
+			for (int i = 0; employee.getLicense_cd().size() > i; i++) {
+				String license_ = employee.getLicense_cd().get(i);
+				license_pstmt.setString(1, license_);
+				try (ResultSet license_res = license_pstmt.executeQuery();) {
+
+					if (license_res.next()) {
+						// m_license
+						license_name.add(license_res.getString("license_name"));
+					}
+				} catch (SQLException e) {
+					throw e;
+				}
+			}
+			employee.setLicense_name(license_name);
+		} catch (SQLException e) {
+			throw e;
+		}
+		return employee;
+	}
+
 	/**
 	 * 従業員情報にかかわる全レコードの取得 ---返り値:ArrayList(EmployeeBean)
-	 *
-	 * @return
 	 */
 	public ArrayList<EmployeeBean> employeeAllGet() {
-		ArrayList<EmployeeBean> emp_list = new ArrayList<EmployeeBean>();
-		EmployeeBean employee = new EmployeeBean();
 
-		employee.setEmp_code("A001");
-		employee.setL_name("田中");
-		employee.setF_name("一郎");
-		employee.setL_kana_name("タナカ");
-		employee.setF_kana_name("イチロウ");
-		employee.setSex((byte) 0);
-		employee.setBirth_day(Date.valueOf("1986-4-20"));
-		employee.setSection_code("01");
-		employee.setEmp_date(Date.valueOf("2010-8-20"));
+		ConnectionManager cm = ConnectionManager.getInstance();
 
-		employee.setSection_name("総務部");
+		ArrayList<EmployeeBean> employeeList = new ArrayList<EmployeeBean>();
 
-		emp_list.add(employee);
+		String emp_sql = "SELECT * FROM m_employee";
+		try (Connection con = cm.getConnection();
+				PreparedStatement emp_pstmt = con.prepareStatement(emp_sql);
+				ResultSet emp_res = emp_pstmt.executeQuery();) {
 
+			while (emp_res.next()) {
+				EmployeeBean employee = new EmployeeBean();
 
-		EmployeeBean employee2 = new EmployeeBean();
-		employee2.setEmp_code("A002");
-		employee2.setL_name("山田");
-		employee2.setF_name("花子");
-		employee2.setL_kana_name("ヤマダ");
-		employee2.setF_kana_name("ハナコ");
-		employee2.setSex((byte) 1);
-		employee2.setBirth_day(Date.valueOf("1976-2-10"));
-		employee2.setSection_code("01");
-		employee2.setEmp_date(Date.valueOf("2003-4-10"));
+				// m_employee
+				employee.setEmp_code(emp_res.getString("emp_code"));
+				employee.setL_name(emp_res.getString("l_name"));
+				employee.setF_name(emp_res.getString("f_name"));
+				employee.setL_kana_name(emp_res.getString("l_kana_name"));
+				employee.setF_kana_name(emp_res.getString("f_kana_name"));
+				employee.setSex(emp_res.getByte("sex"));
+				employee.setBirth_day(CheckFormat.convertDate2String(emp_res.getDate("birth_day")) );
+				employee.setSection_code(emp_res.getString("section_code"));
+				employee.setEmp_date(CheckFormat.convertDate2String(emp_res.getDate("emp_date")) );
+				employee.setUpdate_date(emp_res.getTimestamp("update_date"));
 
-		employee2.setSection_name("営業部");
+				employee = m_sectionGet(con, employee);
+				employee = t_get_licenseGet(con, employee);
+				employee = m_licenseGet(con, employee);
 
-		emp_list.add(employee2);
+				employeeList.add(employee);
+			}
 
-		return emp_list;
+		} catch (SQLException e) {
+			System.out.println("error:EmployeeDAO-employeeAllGet_Prepared");
+			e.printStackTrace();
+		} finally {
+			cm.closeConnection();
+		}
+		return employeeList;
 	}
 
 	/**
 	 * 従業員情報の登録 ---Bean内のm_employeeとt_get_licenseの情報のみ登録されます
 	 *
 	 * @param employee
-	 * @throws ServletServiceException
 	 */
 	public void employeeInsert(EmployeeBean employee) throws ServletServiceException {
+
+		CheckFormat.checkEmployeeBean(employee);
+
+		ArrayList<EmployeeBean> emp_all_list = new EmployeeDAO().employeeAllGet();
+		if (!(CheckFormat.checkPK_empCode(employee, emp_all_list))) {
+			throw new ServletServiceException("従業員コードが重複しています");
+		}
+
+		ConnectionManager cm = ConnectionManager.getInstance();
+
+		String m_emp_sql = "INSERT INTO m_employee (emp_code,l_name,f_name,l_kana_name,f_kana_name,sex,birth_day,section_code,emp_date) VALUES (?,?,?,?,?,?,?,?,?)";
+		String t_get_license_sql = "INSERT INTO t_get_license (emp_code,license_cd,get_license_date) VALUES (?,?,?)";
+
+		try (Connection con = cm.getConnection();
+				PreparedStatement emp_pstmt = con.prepareStatement(m_emp_sql);
+				) {
+			try {
+				con.setAutoCommit(false);
+
+				// m_employee
+				emp_pstmt.setString(1, employee.getEmp_code());
+				emp_pstmt.setString(2, employee.getL_name());
+				emp_pstmt.setString(3, employee.getF_name());
+				emp_pstmt.setString(4, employee.getL_kana_name());
+				emp_pstmt.setString(5, employee.getF_kana_name());
+				emp_pstmt.setByte(6, employee.getSex());
+				emp_pstmt.setDate(7, CheckFormat.convertString2Date(employee.getBirth_day()) );
+				emp_pstmt.setString(8, employee.getSection_code());
+				emp_pstmt.setDate(9, CheckFormat.convertString2Date(employee.getEmp_date()) );
+
+				emp_pstmt.executeUpdate();
+
+				if((employee.getEmp_code() != null) && (employee.getLicense_cd_SQLinsert() != null) && (employee.getGet_license_date_SQLinsert() != null)){
+				try(PreparedStatement get_license_pstmt = con.prepareStatement(t_get_license_sql);){
+					// t_get_license
+					get_license_pstmt.setString(1, employee.getEmp_code());
+					get_license_pstmt.setString(2, employee.getLicense_cd_SQLinsert());
+					get_license_pstmt.setDate(3, CheckFormat.convertString2Date(employee.getGet_license_date_SQLinsert()) );
+
+					get_license_pstmt.executeUpdate();
+				} catch (SQLException e) {
+					throw e;
+				}
+				}
+
+
+				con.commit();
+			} catch (SQLException e) {
+				System.out.println("error:EmployeeDAO-employeeRegist-commit:error");
+				e.printStackTrace();
+				con.rollback();
+				throw e;
+			} finally {
+				con.setAutoCommit(true);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:EmployeeDAO-employeeRegist");
+			e.printStackTrace();
+		} finally {
+			cm.closeConnection();
+		}
 
 	}
 
 	/**
-	 * 従業員情報の更新 ---Bean内のm_employeeとt_get_licenseの、emp_code以外の情報のみ更新します
-	 *
+	 * 従業員情報の更新及び、資格保有情報の追加 ---Bean内のm_employeeとt_get_licenseの、emp_code以外の情報のみ更新します
+	 * 保有資格を追加するためには、一覧から取得したEmployeeBeanの_SQLinsertにデータを格納してこのメソッドに渡してください。
 	 * @param employee
-	 * @throws ServletServiceException
 	 */
 	public void employeeUpdate(EmployeeBean employee) throws ServletServiceException {
+
+		ArrayList<EmployeeBean> emp_all_list = new EmployeeDAO().employeeAllGet();
+		if (CheckFormat.checkPK_empCode(employee, emp_all_list)) {
+			throw new ServletServiceException("従業員コードが存在しません");
+		}
+
+		boolean flag = true;
+		if (CheckFormat.checkPK_t_get_license(employee, emp_all_list)) {
+			flag = true;
+		} else {
+			flag = false;
+		}
+
+		ConnectionManager cm = ConnectionManager.getInstance();
+
+		String m_emp_sql = "UPDATE m_employee SET l_name = ?, f_name = ?, l_kana_name = ?, f_kana_name = ?, sex = ?, birth_day = ?, section_code = ?, emp_date = ? WHERE emp_code = ?";
+		String t_get_license_sql = "INSERT INTO t_get_license (emp_code,license_cd,get_license_date) VALUES (?,?,?)";
+		try (Connection con = cm.getConnection();
+				PreparedStatement emp_pstmt = con.prepareStatement(m_emp_sql);
+				) {
+			try {
+				con.setAutoCommit(false);
+
+				// m_employee
+				emp_pstmt.setString(1, employee.getL_name());
+				emp_pstmt.setString(2, employee.getF_name());
+				emp_pstmt.setString(3, employee.getL_kana_name());
+				emp_pstmt.setString(4, employee.getF_kana_name());
+				emp_pstmt.setByte(5, employee.getSex());
+				emp_pstmt.setDate(6, CheckFormat.convertString2Date(employee.getBirth_day()) );
+				emp_pstmt.setString(7, employee.getSection_code());
+				emp_pstmt.setDate(8, CheckFormat.convertString2Date(employee.getEmp_date()) );
+				emp_pstmt.setString(9, employee.getEmp_code());
+
+				emp_pstmt.executeUpdate();
+
+				// t_get_license
+				if ((employee.getEmp_code() != null) && (employee.getLicense_cd_SQLinsert() != null) && (employee.getGet_license_date_SQLinsert() != null) && (flag) ) {
+					try(PreparedStatement get_license_pstmt = con.prepareStatement(t_get_license_sql);){
+						get_license_pstmt.setString(1, employee.getEmp_code());
+						get_license_pstmt.setString(2, employee.getLicense_cd_SQLinsert());
+						get_license_pstmt.setDate(3, CheckFormat.convertString2Date(employee.getGet_license_date_SQLinsert()));
+						get_license_pstmt.executeUpdate();
+					}catch (SQLException e) {
+						throw e;
+					}
+				}
+
+
+				con.commit();
+			} catch (SQLException e) {
+				System.out.println("error:EmployeeDAO-employeeRegist-commit:error");
+				e.printStackTrace();
+				con.rollback();
+				throw e;
+			} finally {
+				con.setAutoCommit(true);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:EmployeeDAO-employeeRegist");
+			e.printStackTrace();
+		} finally {
+			cm.closeConnection();
+		}
 
 	}
 
@@ -78,10 +308,56 @@ public class EmployeeDAO {
 	 * 従業員情報の削除 ---m_employeeとt_get_licenseから、Bean内のemp_idを元に情報を削除します
 	 *
 	 * @param employee
-	 * @throws ServletServiceException
 	 */
 	public void employeeDelete(EmployeeBean employee) throws ServletServiceException {
 
+		CheckFormat.checkEmployeeBean(employee);
+
+		ArrayList<EmployeeBean> emp_all_list = new EmployeeDAO().employeeAllGet();
+		if (CheckFormat.checkPK_empCode(employee, emp_all_list)) {
+			throw new ServletServiceException("従業員コードが存在しません");
+		}
+
+		ConnectionManager cm = ConnectionManager.getInstance();
+
+		String m_emp_sql = "DELETE FROM m_employee WHERE emp_code = ?";
+		String t_get_license_sql = "DELETE FROM t_get_license WHERE emp_code = ? ";
+
+		try (Connection con = cm.getConnection();
+				PreparedStatement emp_pstmt = con.prepareStatement(m_emp_sql);
+				PreparedStatement get_license_pstmt = con.prepareStatement(t_get_license_sql);) {
+			try {
+				con.setAutoCommit(false);
+
+				// m_employee
+				emp_pstmt.setString(1, employee.getEmp_code());
+
+				// t_get_license
+				get_license_pstmt.setString(1, employee.getEmp_code());
+
+				get_license_pstmt.executeUpdate();
+				emp_pstmt.executeUpdate();
+
+				con.commit();
+			} catch (SQLException e) {
+				System.out.println("error:EmployeeDAO-employeeDelete-commit:error");
+				e.printStackTrace();
+				con.rollback();
+				throw e;
+			} finally {
+				con.setAutoCommit(true);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:EmployeeDAO-employeeDelete");
+			e.printStackTrace();
+		} finally {
+			cm.closeConnection();
+		}
+
 	}
+
+
+
 
 }
